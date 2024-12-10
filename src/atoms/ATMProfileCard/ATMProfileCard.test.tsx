4e0f28868ed284c2ca438e02bdf8955114edbfd5
Ankush <jat.ankush@codiotic.com>
2024-12-10 11:17:59 +0530

    testing library issue resolve

--- src/atoms/ATMProfileCard/ATMProfileCard.test.tsx
+++ src/atoms/ATMProfileCard/ATMProfileCard.test.tsx
@@ -1,24 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ATMProfileCard from './ATMProfileCard';
 
 describe('ATMProfileCard Component', () => {
   test('renders the profile image', () => {
-    render(<ATMProfileCard />);
-    const imgElement = screen.getByAltText(/user-image/i);
+    const { getByAltText } = render(<ATMProfileCard />);
+    const imgElement = getByAltText(/user-image/i);
     expect(imgElement).toBeInTheDocument();
     expect(imgElement).toHaveAttribute('src', '/profile.webp');
   });
 
   test('renders the greeting text', () => {
-    render(<ATMProfileCard />);
-    const greetingElement = screen.getByText(/Hi, Reader,/i);
+    const { getByText } = render(<ATMProfileCard />);
+    const greetingElement = getByText(/Hi, Reader,/i);
     expect(greetingElement).toBeInTheDocument();
   });
 
   test('renders the news text', () => {
-    render(<ATMProfileCard />);
-    const newsElement = screen.getByText(/Here's your News!/i);
+    const { getByText } = render(<ATMProfileCard />);
+    const newsElement = getByText(/Here's your News!/i);
     expect(newsElement).toBeInTheDocument();
   });
 });
